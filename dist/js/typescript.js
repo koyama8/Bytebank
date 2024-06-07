f96b56deecf82caabc9f157b856ad109fb09e32f
Matheus Koyama <koyamaCRS@gmail.com>
2024-06-06 21:11:25 -0300

    Arrays e type alias

--- dist/js/typescript.js
+++ dist/js/typescript.js
@@ -1,5 +1,15 @@
+//Tipos Primitivos
 let valor = 3000;
 let nome = "";
 let isPago = false;
 let qualquer = "";
 qualquer = 22;
+//Arrays
+const lista = [];
+lista.push(13, 22.5, 22, 89, 1.58);
+//Tipos Personalizados (Type Alias)
+const novaTransacao = {
+    tipoTransacao: "",
+    data: new Date(),
+    valor: 0
+};
--- src/typescript.ts
+++ src/typescript.ts
@@ -1,6 +1,25 @@
+//Tipos Primitivos
 let valor:number = 3000;
 let nome:string = "";
 let isPago: boolean = false;
 let qualquer: any =  "";
 qualquer = 22;
 
+//Arrays
+const lista: number[] = [];
+lista.push(13,22.5,22,89,1.58);
+
+
+
+//Tipos Personalizados (Type Alias)
+type Transacao {
+    tipoTransacao:string;
+    data:Date;
+    valor:number;
+}
+
+const novaTransacao: Transacao = {
+    tipoTransacao: "",
+    data: new Date(),
+    valor:0
+}
