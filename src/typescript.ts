b26ceefa81f2ce840622bd795b341497fb7ae374
Matheus Koyama <koyamaCRS@gmail.com>
2024-06-06 21:17:09 -0300

    Enums

--- src/typescript.ts
+++ src/typescript.ts
@@ -13,13 +13,20 @@ lista.push(13,22.5,22,89,1.58);
 
 //Tipos Personalizados (Type Alias)
 type Transacao {
-    tipoTransacao:string;
+    tipoTransacao:TipoTransacao;
     data:Date;
     valor:number;
 }
 
+//Enum
+enum TipoTransacao{
+    DEPOSITO = "Depósito",
+    TRANSFERENCIA = "Transfência",
+    PAGAMENTO_BOLETO = "Pagamento de Boleto"
+}
+
 const novaTransacao: Transacao = {
-    tipoTransacao: "",
+    tipoTransacao: TipoTransacao.PAGAMENTO_BOLETO,
     data: new Date(),
     valor:0
 }
