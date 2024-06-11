f97a9d9cf111f6b2272fac75c1d43e8de70870bf
Matheus Koyama <koyamaCRS@gmail.com>
2024-06-11 19:24:36 -0300

    Formatação de Moeda e Data

--- dist/js/saldo-component.js
+++ dist/js/saldo-component.js
@@ -1,6 +1,16 @@
 let saldo = 3000;
 alert("Testando compilação do TS");
 const elementoSaldo = document.querySelector(".saldo-valor .valor");
+const elementoDataAcesso = document.querySelector(".block-saldo time");
 if (elementoSaldo != null) {
-    elementoSaldo.textContent = saldo.toString();
+    elementoSaldo.textContent = saldo.toLocaleString("pt-br", { currency: "BRL", style: "currency" });
+}
+if (elementoDataAcesso != null) {
+    const dataAcesso = new Date();
+    elementoDataAcesso.textContent = dataAcesso.toLocaleDateString("pr-br", {
+        weekday: "long",
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric"
+    });
 }
